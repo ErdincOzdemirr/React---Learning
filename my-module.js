5da77fdeca19f03aca74be1d1bfb26d8c99fac79
Erdinc <erdincozdemir1812@gmail.com>
2023-10-10 08:07:09 +0300

    first lesson

---
+++ my-module.js
@@ -0,0 +1,8 @@
+const hello = (name) => {
+  console.log("Hello! " + name);
+};
+
+const topla = (a, b) => a + b;
+const cikar = (a, b) => a - b;
+export { topla, cikar };
+export default hello
---
+++ test.js
@@ -0,0 +1,10 @@
+// console.log("Hello Node!")
+
+// var slugify = require('slugify')
+// import slugify from "slugify"
+// const title =  slugify('some string bunun arasına tire koyacak', "_")
+
+import hello, {topla, cikar } from "./my-module.js";
+console.log(topla(2, 4));
+console.log(cikar(11, 3));
+hello("Erdinç");
