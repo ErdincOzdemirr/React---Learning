82f5d2c5f8ea15d269d60f2897d4c9d02e169931
erdinc <erdincozdemir1812@gmail.com>
2023-11-08 23:15:36 +0300

    son

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Hello World")
+
+var slugify = require('slugify')
+import slugify from "slugify"
+const title =  slugify('some string bunun arasına tire koyacak', "_")
+
+console.log(title)
--- my-module.js
+++ my-module.js
@@ -1,4 +1,4 @@
-const hello = (name) => {
+const hello = name => {
   console.log("Hello! " + name);
 };
 
