31f0550dab4bf31286f52787b69d9ae6fcbf0e2f
erdinc <erdincozdemir1812@gmail.com>
2023-11-09 08:00:23 +0300

    node

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-// setTimeout(() => {
-//     console.log("Merhaba")
-// }, 5000)
-
-// setInterval(() => {
-// console.log("Ben Çalışıyorum")
-// },1000)
-
-// const sayHi = (cb) => {
-//     cb();
-// }
-// sayHi(() => {
-//     console.log("Hello")
-// });
-
-// import fetch from "node-fetch";
-// fetch("https://jsonplaceholder.typicode.com/users")
-// .then(data => console.log(data.json))
-// .then((users) => console.log(users));
-
 import axios from 'axios';
 
 // getData fonksiyonunu async olarak tanımlayın ve default olarak dışa aktarın.
