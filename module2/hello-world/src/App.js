ff30d024954f1701c77e7d23a886b85c946745e0
erdinc <erdincozdemir1812@gmail.com>
2023-11-10 08:07:52 +0300

    react

---
+++ module2/hello-world/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/header';
+import React from "react";
+function App(){
+  // return React.createElement('div', null, "Hello")
+  return (
+    <div>
+      <Header/>
+      <Header/>
+      <Header/>
+    </div>
+  )
+
+}
+
+export default App;
---
+++ module2/hello-world/src/components/header.js
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <div>
+            Merhaba Ben Header Bileşeni olarak içeri aktarıldım.
+        </div>
+    )
+}
+
+export default Header;
