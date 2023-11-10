95cce79a7f30daf0cfd7f2f13244e53866caeefa
erdinc <erdincozdemir1812@gmail.com>
2023-11-10 09:34:26 +0300

    react son

--- module2/hello-world/src/App.js
+++ module2/hello-world/src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import Header from './components/header';
 import React from "react";
+
+const name = "Erdinc"
+const surname = "Ozdemir"
+const isLoggedIn = false;
+
 function App(){
   // return React.createElement('div', null, "Hello")
   return (
-    <div>
-      <Header/>
-      <Header/>
-      <Header/>
-    </div>
+    <>
+      {/* <h1>
+     {isLoggedIn && `Benim adım ve soyadım "${name} ${surname}" dir.`}
+      </h1>
+      {!isLoggedIn && "Giris Yapilmadi"} */}
+      <h1>
+        {isLoggedIn ? `Benim adım ve soyadım "${name} ${surname}" dir.` : "Giris Yapilmadi"}
+      </h1>
+    </>
   )
 
 }
